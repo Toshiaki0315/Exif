931dc75834361def9224d2835bcc6629218f8e1e
野村敏昭 <toshiaki0315@gmail.com>
2018-03-21 21:22:28 +0900

    オフセットを保持するように変更

--- Exif.py
+++ Exif.py
@@ -1,17 +1,19 @@
 from ExifParser import AnalyzeExifData
 
 def exif():
+    exif_data = AnalyzeExifData()
+
     with open('./IMG_3294.jpg', 'rb') as infile:
         data = infile.read()
-        offset_base = AnalyzeExifData().check_exif_string(data)
-        endian = AnalyzeExifData().check_byte_order(data, offset_base)
-        offset_0th = AnalyzeExifData().get_0th_offset(endian, data, offset_base)
-        tag_number = AnalyzeExifData().get_tag_number(endian, data, offset_base, offset_0th)
+
+        offset_base = exif_data.check_exif_string(data)
+        exif_data.check_byte_order(data, offset_base)
+        exif_data.get_0th_offset(data, offset_base)
+        tag_number = exif_data.get_tag_number(data, offset_base)
         print('0th IFD tag = {0}'.format(tag_number))
         for count in range(tag_number):
-            tag_info = AnalyzeExifData().get_tag_info(endian, data, offset_base, offset_0th)
+            tag_info = exif_data.get_tag_info(data, offset_base, count*12)
             print('0x{:04x} 0x{:04x} 0x{:08x} 0x{:08x}'.format(tag_info[0],tag_info[1],tag_info[2],tag_info[3]))
-            offset_0th += 12
 
 if __name__ == '__main__':
     exif()
--- ExifParser.py
+++ ExifParser.py
@@ -2,11 +2,13 @@ import struct
 
 class AnalyzeExifData:
     
+    BYTE_ORDER_NONE          = 0
     BYTE_ORDER_LITTLE_ENDIAN = 1
     BYTE_ORDER_BIG_ENDIAN    = 2
-    BYTE_ORDER_ERROR         = 0
+    BYTE_ORDER_ERROR         = -1
 
-    _0th_ifd_offset  = 0
+    _byte_order      = BYTE_ORDER_NONE
+    _0th_ifd_offset  = -1
     _1st_ifd_offset  = 0
     _intr_ifd_offset = 0
     _gps_idf_offset  = 0
@@ -24,36 +26,31 @@ class AnalyzeExifData:
     def check_byte_order(self, data, offset):
         byte_order = struct.unpack_from("2s", data, offset)
         if byte_order[0] == b'MM':
-            return self.BYTE_ORDER_BIG_ENDIAN
+            self._byte_order = self.BYTE_ORDER_BIG_ENDIAN
         elif byte_order[0] == b'II':
-            return self.BYTE_ORDER_LITTLE_ENDIAN
-        return self.BYTE_ORDER_ERROR
+            self._byte_order = self.BYTE_ORDER_LITTLE_ENDIAN
+        else:
+            self._byte_order = self.BYTE_ORDER_ERROR
+        return
 
-    def byte_order(self, endian, data):
-        if endian == self.BYTE_ORDER_BIG_ENDIAN:
-            return int.from_bytes(data, 'big')
-        elif endian == self.BYTE_ORDER_LITTLE_ENDIAN:
-            return int.from_bytes(data, 'little')
-        return -1
+    def get_0th_offset(self, data, offset):
+        if self._byte_order == self.BYTE_ORDER_BIG_ENDIAN:
+            self._0th_ifd_offset = struct.unpack_from(">L", data, offset+4)[0]
+        elif self._byte_order == self.BYTE_ORDER_LITTLE_ENDIAN:
+            self._0th_ifd_offset = struct.unpack_from("<L", data, offset+4)[0]
+        return
 
-    def get_0th_offset(self, endian, data, offset):
-        if endian == self.BYTE_ORDER_BIG_ENDIAN:
-            return struct.unpack_from(">L", data, offset+4)[0]
-        elif endian == self.BYTE_ORDER_LITTLE_ENDIAN:
-            return struct.unpack_from("<L", data, offset+4)[0]
-        return -1
-
-    def get_tag_number(self, endian, data, offset_base, offset_0th_ifd):
-        if endian == self.BYTE_ORDER_BIG_ENDIAN:
-            return struct.unpack_from(">H", data, offset_base+offset_0th_ifd)[0]
-        elif endian == self.BYTE_ORDER_LITTLE_ENDIAN:
-            return struct.unpack_from("<H", data, offset_base+offset_0th_ifd)[0]
+    def get_tag_number(self, data, offset_base):
+        if self._byte_order == self.BYTE_ORDER_BIG_ENDIAN:
+            return struct.unpack_from(">H", data, offset_base+self._0th_ifd_offset)[0]
+        elif self._byte_order == self.BYTE_ORDER_LITTLE_ENDIAN:
+            return struct.unpack_from("<H", data, offset_base+self._0th_ifd_offset)[0]
         return -1
     
-    def get_tag_info(self, endian, data, offset_base, offset_0th_ifd):
-        if endian == self.BYTE_ORDER_BIG_ENDIAN:
-            return struct.unpack_from(">2H2L", data, offset_base+offset_0th_ifd+2)
-        elif endian == self.BYTE_ORDER_LITTLE_ENDIAN:
-            return struct.unpack_from("<2H2L", data, offset_base+offset_0th_ifd+2)
+    def get_tag_info(self, data, offset_base, offset_info):
+        if self._byte_order == self.BYTE_ORDER_BIG_ENDIAN:
+            return struct.unpack_from(">2H2L", data, offset_base+self._0th_ifd_offset+2+offset_info)
+        elif self._byte_order == self.BYTE_ORDER_LITTLE_ENDIAN:
+            return struct.unpack_from("<2H2L", data, offset_base+self._0th_ifd_offset+2+offset_info)
         return -1
         
