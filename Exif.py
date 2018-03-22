b0059701739a643bb32dd5b666bc6e0c1efd56ec
野村敏昭 <toshiaki0315@gmail.com>
2018-03-23 08:05:51 +0900

    1st IFDのオフセットを取得＆リファクタ

--- Exif.py
+++ Exif.py
@@ -1,35 +1,47 @@
 from ExifParser import AnalyzeExifData
 
+def get_0th_ifd(exif_data, data, base_offset, tag_number):
+    first_ifd_offset  = 0
+    exif_ifd_offset = 0
+    gps_ifd_offset  = 0
+    intr_ifd_offset = 0
+
+    print('0th IFD tag = {0}'.format(tag_number))
+    offset = base_offset + 2
+    for count in range(tag_number):
+        tag_info = exif_data.get_tag_info(data, offset)
+        print('0x{:04x} 0x{:04x} 0x{:08x} 0x{:08x}'.format(tag_info[0],tag_info[1],tag_info[2],tag_info[3]))
+        if tag_info[0] == 0x8769:
+            exif_ifd_offset = tag_info[3]
+        elif tag_info[0] == 0x8825:
+            gps_ifd_offset = tag_info[3]
+        elif tag_info[0] == 0xA005:
+            intr_ifd_offset = tag_info[3]
+        offset += 12
+    first_ifd_offset = exif_data.get_1st_ifd_offset(data, offset)
+    
+    return first_ifd_offset, exif_ifd_offset, gps_ifd_offset, intr_ifd_offset
+    
 def exif():
     exif_data = AnalyzeExifData()
 
-    _exif_ifd_offset = 0
-    _gps_ifd_offset  = 0
-    _intr_ifd_offset = 0
     with open('./IMG_3294.jpg', 'rb') as infile:
         data = infile.read()
 
-        offset_base = exif_data.check_exif_string(data)
-        exif_data.check_byte_order(data, offset_base)
-        exif_data.get_0th_offset(data, offset_base)
-        tag_number = exif_data.get_tag_number(data, offset_base)
-        print('0th IFD tag = {0}'.format(tag_number))
-        for count in range(tag_number):
-            tag_info = exif_data.get_tag_info(data, offset_base, count*12)
-            print('0x{:04x} 0x{:04x} 0x{:08x} 0x{:08x}'.format(tag_info[0],tag_info[1],tag_info[2],tag_info[3]))
-            if tag_info[0] == 0x8769:
-                _exif_ifd_offset = tag_info[3]
-            elif tag_info[0] == 0x8825:
-                _gps_ifd_offset = tag_info[3]
-            elif tag_info[0] == 0xA005:
-                _intr_ifd_offset = tag_info[3]
+        base_offset = exif_data.check_exif_string(data)
+        exif_data.check_byte_order(data, base_offset)
+        exif_data.get_0th_offset(data, base_offset)
+        tag_number = exif_data.get_tag_number(data, base_offset)
 
-        if _exif_ifd_offset > 0:
-            print('_exif_ifd_offset = 0x{:08x}'.format(_exif_ifd_offset))
-        if _gps_ifd_offset > 0:
-            print('_gps_ifd_offset  = 0x{:08x}'.format(_gps_ifd_offset))
-        if _intr_ifd_offset > 0:
-            print('_intr_ifd_offset = 0x{:08x}'.format(_intr_ifd_offset))
+        offset = get_0th_ifd(exif_data, data, base_offset, tag_number)
+        if offset[0] > 0:
+            print('1st  IFD Offset = 0x{:08x}'.format(offset[0]))
+        if offset[1] > 0:
+            print('Exif IFD Offset = 0x{:08x}'.format(offset[1]))
+        if offset[2] > 0:
+            print('GPS  IFD Offset = 0x{:08x}'.format(offset[2]))
+        if offset[3] > 0:
+            print('Intr IFD Offset = 0x{:08x}'.format(offset[3]))
 
 if __name__ == '__main__':
     exif()
--- ExifParser.py
+++ ExifParser.py
@@ -2,10 +2,10 @@ import struct
 
 class AnalyzeExifData:
     
-    BYTE_ORDER_NONE          = 0
-    BYTE_ORDER_LITTLE_ENDIAN = 1
-    BYTE_ORDER_BIG_ENDIAN    = 2
-    BYTE_ORDER_ERROR         = -1
+    BYTE_ORDER_NONE          = ""
+    BYTE_ORDER_LITTLE_ENDIAN = "<"
+    BYTE_ORDER_BIG_ENDIAN    = ">"
+    BYTE_ORDER_ERROR         = "ERR"
 
     _byte_order      = BYTE_ORDER_NONE
     _0th_ifd_offset  = -1
@@ -31,27 +31,20 @@ class AnalyzeExifData:
             self._byte_order = self.BYTE_ORDER_LITTLE_ENDIAN
         else:
             self._byte_order = self.BYTE_ORDER_ERROR
+            
         return
 
     def get_0th_offset(self, data, offset):
-        if self._byte_order == self.BYTE_ORDER_BIG_ENDIAN:
-            self._0th_ifd_offset = struct.unpack_from(">L", data, offset+4)[0]
-        elif self._byte_order == self.BYTE_ORDER_LITTLE_ENDIAN:
-            self._0th_ifd_offset = struct.unpack_from("<L", data, offset+4)[0]
+        self._0th_ifd_offset = struct.unpack_from(self._byte_order+"L", data, offset+4)[0]
         return
 
     def get_tag_number(self, data, offset_base):
-        if self._byte_order == self.BYTE_ORDER_BIG_ENDIAN:
-            return struct.unpack_from(">H", data, offset_base+self._0th_ifd_offset)[0]
-        elif self._byte_order == self.BYTE_ORDER_LITTLE_ENDIAN:
-            return struct.unpack_from("<H", data, offset_base+self._0th_ifd_offset)[0]
-        return -1
+        return struct.unpack_from(self._byte_order+"H", data, offset_base+self._0th_ifd_offset)[0]
     
-    def get_tag_info(self, data, offset_base, offset_info):
-        if self._byte_order == self.BYTE_ORDER_BIG_ENDIAN:
-            return struct.unpack_from(">2H2L", data, offset_base+self._0th_ifd_offset+2+offset_info)
-        elif self._byte_order == self.BYTE_ORDER_LITTLE_ENDIAN:
-            return struct.unpack_from("<2H2L", data, offset_base+self._0th_ifd_offset+2+offset_info)
-        return -1
+    def get_tag_info(self, data, offset_base):
+        return struct.unpack_from(self._byte_order+"2H2L", data, offset_base+self._0th_ifd_offset)
+    
+    def get_1st_ifd_offset(self, data, offset):
+        return struct.unpack_from(self._byte_order+"L", data, offset+self._0th_ifd_offset)[0]
 
         
