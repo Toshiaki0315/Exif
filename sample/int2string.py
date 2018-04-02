fcc8d3707a4a69504eeb2c1da9017bb562f3f1d0
Toshiaki Nomura <toshiaki0315@gmail.com>
2018-04-02 20:52:30 +0900

    Test2

--- sample/int2string.py
+++ sample/int2string.py
@@ -10,6 +10,7 @@ class Int2StringTest(unittest.TestCase):
     def test_int_to_string(self):
         self.assertEqual( Int2String().int_to_string( 0x30313233 ), "0123" )
         self.assertEqual( Int2String().int_to_string( 0x61626364 ), "abcd" )
+        self.assertEqual( Int2String().int_to_string( 0x41424344 ), "ABCD" )
 
 if __name__ == '__main__':
     unittest.main()
